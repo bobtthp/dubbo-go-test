84fb7dcf426c8ee06b8fcc23adecca19f49b10cd
gzqtianc <gzqtianc@gmail.com>
2021-12-26 23:40:09 +0800

    add routeer

--- consumer-app/cmd/client.go
+++ consumer-app/cmd/client.go
@@ -19,7 +19,8 @@ package main
 
 import (
 	"context"
-	"time"
+	//"github.com/gin-gonic/gin"
+	//"net/http"
 )
 
 import (
@@ -34,14 +35,76 @@ import (
 
 var grpcGreeterImpl = new(api.GreeterClientImpl)
 
+const MeshRouteConf = "apiVersion: service.dubbo.apache.org/v1alpha1\n" +
+	"kind: DestinationRule\n" +
+	"metadata: { name: demo-route }\n" +
+	"spec:\n" +
+	"  host: demo\n" +
+	"  subsets:\n" +
+	"    - labels: { env-sign: xxx, tag1: hello }\n" +
+	"      name: isolation\n" +
+	"    - labels: { env-sign: yyy }\n" +
+	"      name: testing-trunk\n" +
+	"    - labels: { env-sign: zzz }\n" +
+	"      name: testing\n" +
+	"  trafficPolicy:\n" +
+	"    loadBalancer: { simple: ROUND_ROBIN }\n\n" +
+	"---\n\n" +
+	"apiVersion: service.dubbo.apache.org/v1alpha1\n" +
+	"kind: VirtualService\n" +
+	"metadata: {name: demo-route}\n" +
+	"spec:\n" +
+	"  dubbo:\n" +
+	"    - routedetail:\n" +
+	"        - match:\n" +
+	"            - sourceLabels: {trafficLabel: xxx}\n" +
+	"          name: xxx-project\n" +
+	"          route:\n" +
+	"            - destination: {host: demo, subset: isolation}\n" +
+	"        - match:\n" +
+	"            - sourceLabels: {trafficLabel: testing-trunk}\n" +
+	"          name: testing-trunk\n" +
+	"          route:\n" +
+	"            - destination: {host: demo, subset: testing-trunk}\n" +
+	"        - name: testing\n" +
+	"          route:\n" +
+	"            - destination: {host: demo, subset: testing}\n" +
+	"      services:\n" +
+	"        - {exact: com.apache.dubbo.sample.basic.IGreeter}\n" +
+	"  hosts: [demo]"
+
 // export DUBBO_GO_CONFIG_PATH= PATH_TO_SAMPLES/helloworld/go-client/conf/dubbogo.yml
 func main() {
 	config.SetConsumerService(grpcGreeterImpl)
 	if err := config.Load(); err != nil {
 		panic(err)
 	}
+	// cofig center
+	dynamicConfiguration, err := config.GetRootConfig().ConfigCenter.GetDynamicConfiguration()
+	if err != nil {
+		panic(err)
+	}
 
-	time.Sleep(30 * time.Second)
+	// publish mesh route config
+	err = dynamicConfiguration.PublishConfig("dubbo.io.MESHAPPRULE", "dubbo", MeshRouteConf)
+	if err != nil {
+		return
+	}
+	//// 1.创建路由
+	//r := gin.Default()
+	//// 2.绑定路由规则，执行的函数
+	//// gin.Context，封装了request和response
+	//r.GET("/", func(c *gin.Context) {
+	//	c.String(http.StatusOK, "hello World!")
+	//})
+	//// health check
+	//r.GET("/health.check", func(c *gin.Context) {
+	//	c.String(http.StatusOK, "ok")
+	//})
+	//// 3.监听端口，默认在8080
+	//// Run("里面不指定端口号默认为8080")
+	//r.Run(":8001")
+
 	logger.Info("start to test dubbo")
 	req := &api.HelloRequest{
 		Name: "laurence",
